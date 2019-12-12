fb1354fde98d2767d79e283667929b37f174fbc3
dudemanguy <random342@airmail.cc>
2019-12-12 09:18:15 -0600

    move some curses rendering stuff to separate file
    Not really complicated enough to be called a "refactor" but it's nice to
    move some of the curses related function out of main into separate
    files. We can easily add more general curses stuff here in the future.

---
+++ include/render.h
@@ -0,0 +1,6 @@
+ITEM **create_meta_items(char ***metadata);
+ITEM **create_path_items(char ***entries);
+void create_visual_window(struct vmn_library *lib);
+void destroy_last_menu_meta(struct vmn_library *lib);
+void destroy_last_menu_path(struct vmn_library *lib);
+void destroy_visual_window(struct vmn_library *lib);
--- src/main.c
+++ src/main.c
@@ -18,10 +18,9 @@
 #include "config.h"
 #include "library.h"
 #include "command.h"
+#include "render.h"
 #include "utils.h"
 
-ITEM **create_meta_items(char ***metadata);
-ITEM **create_path_items(char ***files);
 void input_mode(struct vmn_config *cfg);
 char ***get_lib_dir(const char *library, struct vmn_library *lib);
 char ***get_metadata(struct vmn_config *cfg, struct vmn_library *lib);
@@ -200,83 +199,6 @@ int main(int argc, char *argv[]) {
 	}
 	endwin();
 	return 0;
-}
-
-void create_visual_window(struct vmn_library *lib) {
-	lib->visual = newwin(1, 0, LINES - 1, 0);
-	mvwprintw(lib->visual, 0, 0, "-- VISUAL --\n");
-	wrefresh(lib->visual);
-}
-
-ITEM **create_meta_items(char ***metadata) {
-	ITEM **items;
-	int n = 0;
-	while (metadata[0][n]) {
-		++n;
-	}
-	items = (ITEM **)calloc(n+1, sizeof(ITEM *));
-	for (int i = 0; i < n; ++i) {
-		items[i] = new_item(metadata[0][i], metadata[1][i]);
-	}
-	items[n] = (ITEM *)NULL;
-	return items;
-}
-
-ITEM **create_path_items(char ***entries) {
-	ITEM **items;
-	int n = 0;
-	while (entries[0][n]) {
-		++n;
-	}
-	items = (ITEM **)calloc(n+1, sizeof(ITEM *));
-	for (int i = 0; i < n; ++i) {
-		items[i] = new_item(entries[0][i], entries[1][i]);
-	}
-	items[n] = (ITEM *)NULL;
-	return items;
-}
-
-void destroy_last_menu_meta(struct vmn_library *lib) {
-	int n = item_count(lib->menu[lib->depth-1]);
-	unpost_menu(lib->menu[lib->depth-1]);
-	wrefresh(menu_win(lib->menu[lib->depth-1]));
-	delwin(menu_win(lib->menu[lib->depth-1]));
-	free_menu(lib->menu[lib->depth-1]);
-	for (int i = 0; i < n; ++i) {
-		free_item(lib->items[lib->depth-1][i]);
-		free(lib->entries[lib->depth-1][0][i]);
-		free(lib->entries[lib->depth-1][1][i]);
-	}
-	free(lib->items[lib->depth-1]);
-	free(lib->entries[lib->depth-1][0]);
-	free(lib->entries[lib->depth-1][1]);
-	free(lib->entries[lib->depth-1]);
-}
-
-void destroy_last_menu_path(struct vmn_library *lib) {
-	int n = item_count(lib->menu[lib->depth-1]);
-	unpost_menu(lib->menu[lib->depth-1]);
-	wrefresh(menu_win(lib->menu[lib->depth-1]));
-	delwin(menu_win(lib->menu[lib->depth-1]));
-	free_menu(lib->menu[lib->depth-1]);
-	for (int i = 0; i < n; ++i) {
-		free_item(lib->items[lib->depth-1][i]);
-		free(lib->entries[lib->depth-1][0][i]);
-		free(lib->entries[lib->depth-1][1][i]);
-	}
-	free(lib->entries[lib->depth-1][0]);
-	free(lib->entries[lib->depth-1][1]);
-	free(lib->entries[lib->depth-1]);
-	free(lib->items[lib->depth-1]);
-}
-
-void destroy_visual_window(struct vmn_library *lib) {
-	delwin(lib->visual);
-	for (int i = 0; i < lib->depth; ++i) {
-		unpost_menu(lib->menu[i]);
-		post_menu(lib->menu[i]);
-		wrefresh(menu_win(lib->menu[i]));
-	}
 }
 
 char ***get_lib_dir(const char *library, struct vmn_library *lib) {
